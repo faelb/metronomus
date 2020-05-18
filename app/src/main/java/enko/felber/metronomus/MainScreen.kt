4526899554655c51188c788d25d2e89fd47b15d0
faelb <faelb@gmx.at>
2020-05-18 11:06:24 +0200

    bugfixing layout inflater
    layout inflater war id inflater

--- app/src/main/java/enko/felber/metronomus/MainScreen.kt
+++ app/src/main/java/enko/felber/metronomus/MainScreen.kt
@@ -12,13 +12,15 @@ import enko.felber.metronomus.databinding.FragmentMainScreenBinding
  * A simple [Fragment] subclass.
  */
 class MainScreen : Fragment() {
+    //lateinit var binding:FragmentMainScreenBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        val binding:FragmentMainScreenBinding = DataBindingUtil.inflate(inflater,R.id.mainScreen,container,false)
+       val binding:FragmentMainScreenBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_main_screen,container,false)
+
 
         return binding.root
     }
