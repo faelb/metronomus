ea155c876afdea8234ac491537487e843c343c8e
faelb <faelb@gmx.at>
2020-06-15 11:19:01 +0200

    active inactive balls

--- app/src/main/java/enko/felber/metronomus/MainScreen.kt
+++ app/src/main/java/enko/felber/metronomus/MainScreen.kt
@@ -1,5 +1,6 @@
 package enko.felber.metronomus
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -23,6 +24,10 @@ class MainScreen : Fragment() {
        val binding:FragmentMainScreenBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_main_screen,container,false)
 
         binding.imageViewPlayPause.setOnClickListener { it.findNavController().navigate(R.id.action_mainScreen_to_testFragment) }
+
+        //trigger inf clock function for color change of circle
+        binding.buttonPlusOne.setOnClickListener { binding.clock1.setBackgroundResource(R.drawable.rounded_textview_active) }
+
 
         return binding.root
     }
