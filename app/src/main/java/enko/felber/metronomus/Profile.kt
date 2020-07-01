c924401e78e5511b351c0d498b61d686f8678ffa
roben <enkoroberto@gmail.com>
2020-07-01 22:51:20 +0200

    Card Layout angepasst

--- app/src/main/java/enko/felber/metronomus/Profile.kt
+++ app/src/main/java/enko/felber/metronomus/Profile.kt
@@ -7,13 +7,14 @@ class Profile(val pName: String, val pBpm: Int) {
         fun getProfileList(): List<Profile> {
             var profileList: ArrayList<Profile> = ArrayList()
 
-            profileList.add(Profile("Test_1", 100))
-            profileList.add(Profile("Test_2", 100))
-            profileList.add(Profile("Test_3", 100))
-            profileList.add(Profile("Test_4", 100))
-            profileList.add(Profile("Test_5", 100))
-            profileList.add(Profile("Test_6", 100))
-            profileList.add(Profile("Test_7", 100))
+            profileList.add(Profile("Largo", 50))
+            profileList.add(Profile("Larghetto", 65))
+            profileList.add(Profile("Adagio", 70))
+            profileList.add(Profile("Andante", 90))
+            profileList.add(Profile("Moderato", 110))
+            profileList.add(Profile("Allegro", 130))
+            profileList.add(Profile("Presto", 180))
+            profileList.add(Profile("Prestissimo", 200))
 
             return profileList
         }
